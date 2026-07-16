41d543ec1eeacd0c9401d9a75f08660c3ef03002
Raghavendra Singh <raghavendrashivam474@gmail.com>
2026-07-16 21:22:25 +0530

    chore: bump version to 0.9.0

--- rshx/__init__.py
+++ rshx/__init__.py
@@ -5,7 +5,7 @@ RSHX - Raghav Shell eXtended.
 Top-level package. Single authoritative version source.
 """
 
-__version__ = "0.8.0"
+__version__ = "0.9.0"
 __author__ = "Raghavendra Singh"
 __description__ = "RSHX - Raghav Shell eXtended"
 __license__ = "MIT"
